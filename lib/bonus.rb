d8310d495537a9a4bce8e3eaf7ffa16c929449c2
Kish Siva <rodes88@msn.com>
2019-04-13 02:43:27 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -27,7 +27,7 @@ puts epic_tragedy
 
   #code your solution here:
 
-  
+  epic_tragedy[]
   
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
